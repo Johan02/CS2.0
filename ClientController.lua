9157b2e6135ad77bf207b908e17ce186cc931324
Johan02 <johandif3@gmail.com>
2021-09-07 11:11:35 +0200

    Added basic functions

--- ClientController.lua
+++ ClientController.lua
@@ -10,15 +10,21 @@ local Settings = require(Folder.Settings)
 
 local Character = Player.Character
 
+local Humanoid = Character.Humanoid
+local HumanoidRootPart = Character.HumanoidRootPart
+
+local BodyVelocity = HumanoidRootPart:WaitForChild('BodyVelocity')
 
 UIS.InputBegan:Connect(function(Input, GameProcessing)
 
     if not GameProcessing then
         
-        if Input.KeyCode == Settings.WalkForward then
-            
-        end
+        if Humanoid then
 
+            if Input.KeyCode == Settings.WalkForward then
+                
+            end
+        end
     end
 
 
